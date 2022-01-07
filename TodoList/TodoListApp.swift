6e16ddc19a8b5113449de64c31c760d355bbdc8e
Ajay Kumar <ajaykhatri2123@Outlook.com>
2022-01-07 01:38:34 +0100

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Ajay Kumar on 07/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
